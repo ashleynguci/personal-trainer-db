f3890f79968916150a717ce3c48c85a177ed551b
ashleynguci <trucanhftu2@gmail.com>
2019-07-08 14:22:25 +0300

    Updates

---
+++ precache-manifest.2297c1d3d18bc01b393417f41da2d86a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b12c7d128cd0593b0266",
+    "url": "/personal-trainer-db/static/js/runtime~main.3405e75a.js"
+  },
+  {
+    "revision": "fcd496a9f5fe2d66d07d",
+    "url": "/personal-trainer-db/static/js/main.fe4dd346.chunk.js"
+  },
+  {
+    "revision": "54653e03f1203f818d46",
+    "url": "/personal-trainer-db/static/js/2.d9ffd5b5.chunk.js"
+  },
+  {
+    "revision": "fcd496a9f5fe2d66d07d",
+    "url": "/personal-trainer-db/static/css/main.d1e4e308.chunk.css"
+  },
+  {
+    "revision": "54653e03f1203f818d46",
+    "url": "/personal-trainer-db/static/css/2.b8c0820c.chunk.css"
+  },
+  {
+    "revision": "1b2aec58fd127ad727440a7d7a120a06",
+    "url": "/personal-trainer-db/index.html"
+  }
+];
